24a63fbb79c421a6db7d39d4184bc872d522fa3e
Demetriy <ArhDemetriy@GMail.com>
2021-04-27 20:28:13 +0800

    ref: move code from index to DemosManager

---
+++ src/components/complicated/index/DemosManager.ts
@@ -0,0 +1,34 @@
+import { Destructible } from 'components/type'
+
+class DemosManager implements Destructible {
+  protected readonly mainElement: HTMLElement
+  protected readonly addButton: HTMLButtonElement
+  protected readonly template: HTMLTemplateElement
+  protected readonly listDemos: HTMLDivElement
+
+  private readonly bindedAddNewDemo = this.addNewDemo.bind(this);
+  constructor(mainElement: HTMLElement, mainClass: string = 'index') {
+    this.mainElement = mainElement
+    this.addButton = mainElement.querySelector(`.${mainClass}__button--add_template`)
+    this.template = mainElement.querySelector(`.${mainClass}__template`)
+    this.listDemos = mainElement.querySelector(`.${mainClass}__list_demo`)
+
+    this.addEventsListeners()
+  }
+  private addNewDemo(event: MouseEvent) {
+    const clone = document.importNode(this.template.content, true)
+    const nevSlider = clone.querySelector<HTMLDivElement>('.slider')
+
+    this.listDemos.appendChild(clone)
+  }
+  protected addEventsListeners() {
+    this.addButton.addEventListener('click', this.bindedAddNewDemo)
+  }
+  destroy() {
+    this.addButton.removeEventListener('click', this.bindedAddNewDemo)
+  }
+}
+
+export {
+  DemosManager,
+}
--- src/components/complicated/index/index.ts
+++ src/components/complicated/index/index.ts
@@ -1,34 +1,9 @@
-import { Destructible } from 'components/type'
+import { Destructible } from "baseType";
+import { DemosManager } from "comp/index/DemosManager";
 
-class Index implements Destructible {
-  protected readonly mainElement: HTMLElement
-  protected readonly addButton: HTMLButtonElement
-  protected readonly template: HTMLTemplateElement
-  protected readonly listDemos: HTMLDivElement
+const classDemosManager = 'index'
+const indexes: Destructible[] = Array
+  .from(document.querySelectorAll<HTMLDivElement>(`.${classDemosManager}`))
+  .map(element => new DemosManager(element, classDemosManager))
 
-  private readonly bindedAddNewDemo = this.addNewDemo.bind(this);
-  constructor(mainElement: HTMLElement, mainClass: string = 'index') {
-    this.mainElement = mainElement
-    this.addButton = mainElement.querySelector(`.${mainClass}__button--add_template`)
-    this.template = mainElement.querySelector(`.${mainClass}__template`)
-    this.listDemos = mainElement.querySelector(`.${mainClass}__list_demo`)
-
-    this.addEventsListeners()
-  }
-  private addNewDemo(event: MouseEvent) {
-    const clone = document.importNode(this.template.content, true)
-    const nevSlider = clone.querySelector<HTMLDivElement>('.slider')
-
-    this.listDemos.appendChild(clone)
-  }
-  protected addEventsListeners() {
-    this.addButton.addEventListener('click', this.bindedAddNewDemo)
-  }
-  destroy() {
-    this.addButton.removeEventListener('click', this.bindedAddNewDemo)
-  }
-}
-
-export {
-  Index,
-}
+export { indexes }
