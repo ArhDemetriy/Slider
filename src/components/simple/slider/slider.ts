6c980257014f703f5970763dd78e8151b9f47e93
Demetriy <ArhDemetriy@GMail.com>
2021-04-29 20:28:15 +0800

    add: move bar

--- src/components/simple/slider/slider.ts
+++ src/components/simple/slider/slider.ts
@@ -1,4 +1,4 @@
-import { Destructible } from '../../type'
+import { Destructible } from 'baseType'
 
 class Slider implements Destructible {
   moveRight() {
@@ -7,12 +7,25 @@ class Slider implements Destructible {
   moveLeft() {
     this.moveBar(-5)
   }
-
+  destroy() {
+    this.removeEventsListeners()
+    this.destructible.forEach(elem => elem.destroy())
+  }
+  protected readonly destructible: Destructible[] = []
+  constructor(mainElement: HTMLElement, mainClass: string = 'slider') {
+    this.mainElement = mainElement
+    this.bar = this.mainElement.querySelector(`.${mainClass}__bar`)
+    this.initComponents()
+    this.addEventsListeners()
+  }
   protected readonly mainElement: HTMLElement
   protected readonly bar: HTMLDivElement
-
-  protected readonly destructible: Destructible[] = []
-
+  protected initComponents() {
+    this.barPosition = 33
+  }
+  protected moveBar(x: number) {
+    this.barPosition += x
+  }
   private _barPosition = 0
   protected get barPosition() {
     return this._barPosition
@@ -21,40 +34,49 @@ class Slider implements Destructible {
     this._barPosition = x
     this.mainElement.style.setProperty('--barPosition', this._barPosition.toString())
   }
-  protected readonly bindedMouseDown = this.mouseDown.bind(this)
-  constructor(mainElement: HTMLElement, mainClass: string = 'slider') {
-    this.mainElement = mainElement
-    this.bar = this.mainElement.querySelector(`.${mainClass}__bar`)
-    this.initComponents()
-    this.addEventsListeners()
-  }
-  protected initComponents() {
-    this.mainElement.style.setProperty('--barPosition', this.barPosition.toString())
+
+  protected removeEventsListeners() {
+    this.mainElement.removeEventListener('mousedown', this.bindedMainMouseOn)
+    this.mainElement.removeEventListener('mouseup', this.bindedMainMouseOff)
   }
   protected addEventsListeners() {
-    this.mainElement.addEventListener('mousedown', this.bindedMouseDown, { passive: true })
+    this.mainElement.addEventListener('mousedown', this.bindedMainMouseOn, { passive: true })
+    this.mainElement.addEventListener('mouseup', this.bindedMainMouseOff, { passive: true })
   }
-  protected moveBar(x: number) {
-    this.barPosition += x
+  protected width: number
+  protected start: number
+  protected min: number
+  protected max: number
+
+  protected readonly bindedMainMouseOn = this.mainMouseOn.bind(this)
+  protected mainMouseOn(ev: MouseEvent) {
+    this.width = Number.parseInt(window.getComputedStyle(this.mainElement).width)
+    this.min = Math.floor(this.mainElement.getBoundingClientRect().left)
+    this.max = this.min + this.width
+
+    this.start = ev.pageX
+
+    document.addEventListener('mousemove', this.bindedMainMouseMove, { passive: true })
+    document.addEventListener('mouseup', this.bindedMainMouseOff, { passive: true, once: true })
   }
-  destroy() {
-    this.mainElement.removeEventListener('mousedown', this.bindedMouseDown)
-    this.destructible.forEach(elem => elem.destroy())
+  protected readonly bindedMainMouseOff = this.mainMouseOff.bind(this)
+  protected mainMouseOff(ev: MouseEvent) {
+    document.removeEventListener('mousemove', this.bindedMainMouseMove)
   }
-
-  protected mouseDown(ev: MouseEvent) {
-    console.log('***********');
-    const local = ev.offsetX
-    console.log(local);
-    const w = window.getComputedStyle(this.mainElement).width
-    console.log(w);
-    console.log($(this.mainElement).css('--barPosition'));
-
-
-
-
-
-
+  protected readonly bindedMainMouseMove = this.mainMouseMove.bind(this)
+  protected mainMouseMove(ev: MouseEvent) {
+    // оптимизация:
+    // т.к.событие происходит очень часто => минимум доп констант.
+    // и this.barPosition геттерится только 1 раз.
+    // если убрать selfX то мы поменяем 1 сумму на два разименования указателя.
+    const selfX = ev.pageX - this.min
+    if (selfX > this.width) {
+      if (this.barPosition <= 100) { this.barPosition = 100 }
+    }else if (selfX < 0) {
+      if (this.barPosition >= 0) { this.barPosition = 0 }
+    } else {
+      this.barPosition = selfX / (this.width / 100)
+    }
   }
 }
 
