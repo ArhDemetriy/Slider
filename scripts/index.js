614bdff8332ff98773fe3c4202224710134af17e
Demetriy <ArhDemetriy@GMail.com>
2021-04-30 15:33:15 +0800

    gh-pages add: bar, mousemove

--- scripts/index.js
+++ scripts/index.js
@@ -121,32 +121,60 @@ var Demo = /*#__PURE__*/function () {
 
     _defineProperty(this, "controls", void 0);
 
+    _defineProperty(this, "leftButton", void 0);
+
+    _defineProperty(this, "rightButton", void 0);
+
+    _defineProperty(this, "sliderElement", void 0);
+
     _defineProperty(this, "slider", void 0);
 
     _defineProperty(this, "destructible", []);
 
-    _defineProperty(this, "bindedEventListener", this.eventListener.bind(this));
+    _defineProperty(this, "bindedMoveLeftEvent", this.moveLeftEvent.bind(this));
+
+    _defineProperty(this, "bindedMoveRightEvent", this.moveRightEvent.bind(this));
 
     this.mainElement = mainElement;
     this.controls = mainElement.querySelector(".".concat(mainClass, "__controls"));
-    this.slider = mainElement.querySelector(".".concat(mainClass, "__slider"));
+    this.leftButton = this.controls.querySelector(".".concat(mainClass, "__left_button"));
+    this.rightButton = this.controls.querySelector(".".concat(mainClass, "__right_button"));
+    this.sliderElement = mainElement.querySelector(".".concat(mainClass, "__slider"));
+    this.slider = new simp_slider_slider__WEBPACK_IMPORTED_MODULE_0__.Slider(this.sliderElement);
     this.initComponents();
+    this.addEventsListeners();
   }
 
   _createClass(Demo, [{
     key: "initComponents",
     value: function initComponents() {
-      this.destructible.push(new simp_slider_slider__WEBPACK_IMPORTED_MODULE_0__.Slider(this.slider));
+      this.destructible.push(this.slider);
     }
   }, {
-    key: "eventListener",
-    value: function eventListener(event) {}
+    key: "moveLeftEvent",
+    value: function moveLeftEvent(event) {
+      this.slider.moveLeft();
+    }
+  }, {
+    key: "moveRightEvent",
+    value: function moveRightEvent(event) {
+      this.slider.moveRight();
+    }
   }, {
     key: "addEventsListeners",
-    value: function addEventsListeners() {}
+    value: function addEventsListeners() {
+      this.leftButton.addEventListener('click', this.bindedMoveLeftEvent, {
+        passive: true
+      });
+      this.rightButton.addEventListener('click', this.bindedMoveRightEvent, {
+        passive: true
+      });
+    }
   }, {
     key: "destroy",
     value: function destroy() {
+      this.leftButton.removeEventListener('click', this.bindedMoveLeftEvent);
+      this.rightButton.removeEventListener('click', this.bindedMoveRightEvent);
       this.destructible.forEach(function (elem) {
         return elem.destroy();
       });
@@ -217,7 +245,9 @@ var DemosManager = /*#__PURE__*/function () {
   }, {
     key: "addEventsListeners",
     value: function addEventsListeners() {
-      this.addButton.addEventListener('click', this.bindedAddNewDemo);
+      this.addButton.addEventListener('click', this.bindedAddNewDemo, {
+        passive: true
+      });
     }
   }, {
     key: "destroy",
@@ -282,39 +312,127 @@ var Slider = /*#__PURE__*/function () {
 
     _classCallCheck(this, Slider);
 
-    _defineProperty(this, "mainElement", void 0);
-
     _defineProperty(this, "destructible", []);
 
-    _defineProperty(this, "iterator", 0);
+    _defineProperty(this, "mainElement", void 0);
 
-    _defineProperty(this, "bindedEventListener", this.eventListener.bind(this));
+    _defineProperty(this, "bar", void 0);
+
+    _defineProperty(this, "_barPosition", 0);
+
+    _defineProperty(this, "width", void 0);
+
+    _defineProperty(this, "start", void 0);
+
+    _defineProperty(this, "min", void 0);
+
+    _defineProperty(this, "max", void 0);
+
+    _defineProperty(this, "bindedMainMouseOn", this.mainMouseOn.bind(this));
+
+    _defineProperty(this, "bindedMainMouseOff", this.mainMouseOff.bind(this));
+
+    _defineProperty(this, "bindedMainMouseMove", this.mainMouseMove.bind(this));
 
     this.mainElement = mainElement;
+    this.bar = this.mainElement.querySelector(".".concat(mainClass, "__bar"));
     this.initComponents();
     this.addEventsListeners();
   }
 
   _createClass(Slider, [{
-    key: "initComponents",
-    value: function initComponents() {}
-  }, {
-    key: "eventListener",
-    value: function eventListener(event) {
-      alert(this.iterator++);
+    key: "moveRight",
+    value: function moveRight() {
+      this.moveBar(5);
     }
   }, {
-    key: "addEventsListeners",
-    value: function addEventsListeners() {
-      this.mainElement.addEventListener('click', this.bindedEventListener);
+    key: "moveLeft",
+    value: function moveLeft() {
+      this.moveBar(-5);
     }
   }, {
     key: "destroy",
     value: function destroy() {
-      this.mainElement.removeEventListener('click', this.bindedEventListener);
+      this.removeEventsListeners();
       this.destructible.forEach(function (elem) {
         return elem.destroy();
       });
+    }
+  }, {
+    key: "initComponents",
+    value: function initComponents() {
+      this.barPosition = 33;
+    }
+  }, {
+    key: "moveBar",
+    value: function moveBar(x) {
+      this.barPosition += x;
+    }
+  }, {
+    key: "barPosition",
+    get: function get() {
+      return this._barPosition;
+    },
+    set: function set(x) {
+      this._barPosition = x;
+      this.mainElement.style.setProperty('--barPosition', this._barPosition.toString());
+    }
+  }, {
+    key: "removeEventsListeners",
+    value: function removeEventsListeners() {
+      this.mainElement.removeEventListener('mousedown', this.bindedMainMouseOn);
+      this.mainElement.removeEventListener('mouseup', this.bindedMainMouseOff);
+    }
+  }, {
+    key: "addEventsListeners",
+    value: function addEventsListeners() {
+      this.mainElement.addEventListener('mousedown', this.bindedMainMouseOn, {
+        passive: true
+      });
+      this.mainElement.addEventListener('mouseup', this.bindedMainMouseOff, {
+        passive: true
+      });
+    }
+  }, {
+    key: "mainMouseOn",
+    value: function mainMouseOn(ev) {
+      this.width = Number.parseInt(window.getComputedStyle(this.mainElement).width, 10);
+      this.min = Math.floor(this.mainElement.getBoundingClientRect().left);
+      this.max = this.min + this.width;
+      this.start = ev.pageX;
+      document.addEventListener('mousemove', this.bindedMainMouseMove, {
+        passive: true
+      });
+      document.addEventListener('mouseup', this.bindedMainMouseOff, {
+        passive: true,
+        once: true
+      });
+    }
+  }, {
+    key: "mainMouseOff",
+    value: function mainMouseOff(ev) {
+      document.removeEventListener('mousemove', this.bindedMainMouseMove);
+    }
+  }, {
+    key: "mainMouseMove",
+    value: function mainMouseMove(ev) {
+      // оптимизация:
+      // т.к.событие происходит очень часто => минимум доп констант.
+      // и this.barPosition геттерится только 1 раз.
+      // если убрать selfX то мы поменяем 1 сумму на два разименования указателя.
+      var selfX = ev.pageX - this.min;
+
+      if (selfX > this.width) {
+        if (this.barPosition <= 100) {
+          this.barPosition = 100;
+        }
+      } else if (selfX < 0) {
+        if (this.barPosition >= 0) {
+          this.barPosition = 0;
+        }
+      } else {
+        this.barPosition = selfX / (this.width / 100);
+      }
     }
   }]);
 
