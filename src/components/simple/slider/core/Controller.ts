367cd0188cb5cea8d463e34cb94dc188fdf07bf0
Demetriy <ArhDemetriy@GMail.com>
2021-05-10 21:14:36 +0800

    ref: types in Controller ref: Controller in UML

--- src/components/simple/slider/core/Controller.ts
+++ src/components/simple/slider/core/Controller.ts
@@ -1,30 +1,23 @@
+import { XOR } from "baseType";
 import { Model, ModelSettings } from "./model/Model";
 import { Model2, Model2Settings } from "./model/Model2";
 import { View, ViewSettings } from "./view/View";
-type XOR<T1 extends { [k in string]: any }, T2 extends { [k in string]: any }> =
-  | ({ [k in keyof T1]: T1[k] } & { [k in Exclude<(keyof T2), (keyof T1)>]?: never })
-  | ({ [k in keyof T2]: T2[k] } & { [k in Exclude<(keyof T1), (keyof T2)>]?: never });
 
-type TMark = number | string
-type oneBar<T> = { initPositions: [T, never], quantityBars?: 1 } | { initPositions?: [T, never], quantityBars: 1 }
-type twoBar<T> = { initPositions: [T, T], quantityBars?: 2 } | { initPositions?: [T, T], quantityBars: 2 }
-type ControllerSettings<T> = {
+type ControllerSettings<Count extends (1 | 2), T extends number | string> = {
   min: T
   max: T
-  quantityBars?: 1 | 2
-  initPositions?: [T] | [T, T]
   promoPoints?: T[]
-}
-
-class Controller<T extends TMark = number> {
-  private readonly quantityBars: 1 | 2
-  constructor(settings: ControllerSettings<T>) {
-    settings.initPositions = settings.initPositions || [settings.min]
-    settings.initPositions[0] = settings.initPositions[0] || settings.min
-    if (settings.quantityBars == 2) {
-      settings.initPositions[1] = settings.initPositions[1] || settings.max
-    }
-    this.quantityBars = settings.quantityBars || settings.initPositions.length
+} & Count extends 1
+  ? {
+    count?: 1
+    initPositions?: [T]
+  } : {
+    count?: 2
+    initPositions?: [T, T?]
+  }
+  // Count extends 1 ? Model : Model2
+class Controller<Count extends (1 | 2), T extends number | string = number> {
+  constructor(settings: ControllerSettings<Count, T>) {
   }
   public getStartPosition() {
 
@@ -32,13 +25,17 @@ class Controller<T extends TMark = number> {
   public getPositions() {
     return [this.getStartPosition()]
   }
-  public getEndPosition(){
-
+  public getEndPosition(): Count extends 1 ? T : Error {
+    throw new TypeError('')
   }
-  protected model: Model<T> | Model2<T>
-  protected view: View<T>
+  protected model: Count extends 1 ? Model<T> : Model2<T>
+  protected view: Count extends 1 ? View<T> : View<T>
   protected initOneBar() { }
   protected initTwoBar() { }
+
 }
 
-export { Controller, ControllerSettings, TMark }
+export {
+  Controller,
+  ControllerSettings,
+}
