cc03919dd0abff5a65c14f97a21120c6b1c99de8
Demetriy <ArhDemetriy@GMail.com>
2021-05-15 16:20:21 +0800

    init Controller tests

---
+++ src/components/simple/slider/core/__tests__/Controller.test.ts
@@ -0,0 +1,5 @@
+import { Controller, ControllerSettings } from "../Controller";
+jest.mock('../model/Model.ts')
+jest.mock('../model/Model2.ts')
+jest.mock('../view/View.ts')
+jest.mock('../view/View2.ts')
