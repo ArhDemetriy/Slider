f470ad5ec5c2028a836f25e2ce234f63392a350f
Demetriy <ArhDemetriy@GMail.com>
2021-05-16 10:49:58 +0800

    add: HTMLElementBase Destructible in View

--- src/components/simple/slider/core/view/View.ts
+++ src/components/simple/slider/core/view/View.ts
@@ -1,17 +1,23 @@
-import { Bars } from "./bars/Bar";
+import { Destructible } from "baseType";
+import { HTMLElementBase, HTMLElementBaseSettings } from "./HTMLElementBase";
+import { Bar } from "./bars/Bar";
 import { Scale } from "./scale/Scale";
-interface ViewSettings<T>{
-  mainElement: HTMLDivElement
+interface ViewSettings<T> extends HTMLElementBaseSettings<HTMLDivElement> {
   min: T
   max: T
   step?: T
   promoPoints?: T
   barsInitPosition?: T
 }
-class View<T> {
+class View<T> extends HTMLElementBase<HTMLDivElement> implements Destructible{
   constructor(settings: ViewSettings<T>) {
+    super(settings)
   }
-  protected bars: Bars
+  destroy() {
+    this.bar.destroy()
+    this.scale.destroy()
+  }
+  protected bar: Bar<T>
   protected scale: Scale
 }
 
